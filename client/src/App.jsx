1117b3d108892d741fed269a487e75118901f5aa
=node <harish99427@gmail.com>
2025-01-01 19:14:07 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/projects' element={<Projects/>} />
       
-    </div>
+    </Routes>
+    </BrowserRouter>
   )
 }
