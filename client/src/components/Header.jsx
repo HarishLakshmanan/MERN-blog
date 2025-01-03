f1eb61e6d28fe8a077f371080f7173b6eced4078
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-01-03 20:15:56 +0530

    create fn in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,40 @@
-import { Navbar } from 'flowbite-react'
+import { Button, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon} from 'react-icons/fa'
 
 function Header() {
+
   return (
-    <div>
-     <Navbar className='border-b-2'>
-       <Link to="/" className='self-center whitespace-nowrap'>
-         <span>Main</span>
+     <Navbar className='border-b-2 bg-white'>
+       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+         <span className='px-2 py-1 bg-gradient-to-r from-bg-indigo-500 via-purple-500
+         to-pink-500 rounded-lg text-white'>Main</span>
          BloG
         </Link> 
+        <form>
+          <TextInput
+            type='text'
+            placeholder='search'
+            rightIcon={AiOutlineSearch}
+            className='hidden lg:inline'
+          />
+        </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch/>
+        </Button>
+        <div>
+          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+            <FaMoon/>
+          </Button>
+          <Link to='/sign-in'>
+          <Button className='w-1 h-10 hidden sm:inline' color='gray' pill>
+            Sing In
+          </Button>
+          </Link>
+        </div>
       </Navbar>
-    </div>
   )
 }
 
