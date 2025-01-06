3e6bc9252602e864a75964c186e8eaa2a11fb48a
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-01-06 19:31:26 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
             <FaMoon/>
           </Button>
           <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' >
+          <Button gradientDuoTone='purpleToBlue' outline >
             Sing In
           </Button>
           </Link>
