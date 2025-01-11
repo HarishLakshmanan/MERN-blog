d7a94d983b9091e39ab6111c4514f086771791d2
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-01-11 18:05:13 +0530

    create signin api page

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/usermodel.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup=async(req,res,next)=>{
     // console.log(req.body);
@@ -24,5 +25,31 @@ export const signup=async(req,res,next)=>{
         
     } catch (error) {
       next(error);
+    }
+};
+
+export const signin =async(req,res,next)=>{
+    const {email,password}=req.body;
+
+    if(!email || !password ||email === '' || password === ''){
+        next(errorHandler(400,'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            next(errorHandler(404,'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+           return next(errorHandler(400,'Invalid password'))
+        }
+        const token = jwt.sign({id:validUser._id},"86e7b1c268a68fa890f9614a084e5cbbd1aa780dbe6676221a2d5e4f4beaff15");
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true,
+        })
+        .json(validUser);
+    } catch (error) {
+        next(error)
     }
 }
