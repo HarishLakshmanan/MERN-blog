9f4de51adb3a8901a4b274763021e16f2a468b93
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-01-15 18:54:14 +0530

    create dashboard elements

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Sidebar } from 'flowbite-react';
+import{HiUser,HiArrowSmRight} from 'react-icons/hi';
+import { useEffect,useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab,setTab]=useState('');
+    useEffect(()=>{
+    const urlParms = new URLSearchParams(location.search);
+    const tabFromUrl = urlParms.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  },[location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active={tab === 'profile'} icon={HiUser} lable={'User'} lableColor='dark'>
+                    profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' >
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
 
 function Dashboard() {
+  const location = useLocation();
+  const [tab,setTab]=useState('');
+  useEffect(()=>{
+    const urlParms = new URLSearchParams(location.search);
+    const tabFromUrl = urlParms.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  },[location.search]);
   return (
-    <div>
-      Dashboard
+    <div className='min-h- h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+       <DashSidebar/>
+      </div>
+      {tab === 'profile' && <DashProfile/>}
     </div>
   )
 }
