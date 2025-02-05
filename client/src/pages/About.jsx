02bbe11f14c4bbc96f09c7d08368c3729b2e8225
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-02-05 22:05:28 +0530

    complete functionalities in about and project pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex flex-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Main BloG</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              welcome to Main BloG! This blog was created
+              by harish, as a personal to shar his thoughts
+              and ideas with the world. Harish is a passionate
+              developer who loves to write about technology,
+              coding,and everything in between 
+            </p>
+            <p>
+            Welcome to our corner of the digital world! We are a passionate team dedicated to
+            bringing you the best experiences through our products and services. With a blend of
+            creativity, innovation, and a commitment to excellence, we strive to make a positive
+            impact on your life. Whether you're here to learn, grow, or simply enjoy, our mission is to
+            empower and inspire you every step of the way. Join us on this exciting journey as we
+            continue to push boundaries and explore new horizons together. Thank you for being a
+            part of our community!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
