6db112e1243a9a89739b3061c801b994f20e5f88
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-02-05 21:42:26 +0530

    add search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,60 @@
 import { Avatar, Button, Dropdown, DropdownHeader, Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link, useLocation} from 'react-router-dom'
-import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon,FaSun} from 'react-icons/fa'
-import {useSelector,useDispatch} from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
 import toogleTheme from '../redux/theme/themeSlice'
 import { signoutSuccess } from '../redux/user/userSlice'
 
 
 
 function Header() {
-   const path = useLocation().pathname;
-   const dispatch = useDispatch();
-   const {currentUser}=useSelector(state => state.user)
-   const {theme} = useSelector((state)=>state.theme);
-   const handleSignout =async ()=>{
-         try {
-           const res =await fetch('/api/user/signout',{
-             method:'POST',
-           });
-           const data =await res.json();
-           if(!res.ok){
-             console.log(data.message)
-           }else{
-             dispatch(signoutSuccess());
-           }
-         } catch (error) {
-           console.log(error.message)
-         }
-       };
+  const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user)
+  const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('')
+
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message)
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
+
+  const handleSubmit = (e)=>{
+     e.preventDefault();
+     const urlParams = new URLSearchParams(location.search);
+     urlParams.set('searchTerm',searchTerm);
+     const searchQuery=urlParams.toString();
+     navigate(`/search?${searchQuery}`);
+  };
+
   return (
-     <Navbar className='border-b-2'>
+    <Navbar className='border-b-2'>
       <Link
         to='/'
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
@@ -40,74 +64,76 @@ function Header() {
         </span>
         BloG
       </Link>
-        <form>
-          <TextInput
-            type='text'
-            placeholder='search'
-            rightIcon={AiOutlineSearch}
-            className='hidden lg:inline'
-          />
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-        <AiOutlineSearch/>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          type='text'
+          placeholder='search'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill
+          onClick={() => dispatch(toogleTheme())}>
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <div className='flex gap-2 md:order-2'>
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill
-          onClick={()=>dispatch(toogleTheme())}>
-            {theme === 'light' ? <FaSun/> : <FaMoon/>}
-          </Button>
-          {currentUser ? (
-            <Dropdown
+        {currentUser ? (
+          <Dropdown
             arrowIcon={false}
             inline
             label={
               <Avatar
-               alt='user'
-               img={currentUser.profilepicture}
-               rounded
+                alt='user'
+                img={currentUser.profilepicture}
+                rounded
               />
             }
-            >
-              <DropdownHeader>
-                <span className='block text-sm'>@{currentUser.username}</span>
-                <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
-              </DropdownHeader>
-              <Link to={'/dashboard?tab=profile'}>
+          >
+            <DropdownHeader>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>@{currentUser.email}</span>
+            </DropdownHeader>
+            <Link to={'/dashboard?tab=profile'}>
               <Dropdown.Item>profile</Dropdown.Item>
-               
-              </Link>
-              <Dropdown.Divider/>
-              <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-            </Dropdown>
-          ):
+
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) :
           (
             <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            SingIn
-          </Button>
-          </Link>
+              <Button gradientDuoTone='purpleToBlue' outline>
+                SingIn
+              </Button>
+            </Link>
           )
-       }
-          <Navbar.Toggle/>
-        </div>
-          <Navbar.Collapse>
-            <Navbar.Link active={path === '/'} as={'div'}>
-              <Link to='/'>
-                Home
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/about'}as={'div'}>
-              <Link to='/about'>
-                About
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === '/projects'} as={'div'}>
-              <Link to='/projects'>
-                Project
-              </Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
-      </Navbar>
+        }
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>
+            Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>
+            About
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>
+            Project
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
