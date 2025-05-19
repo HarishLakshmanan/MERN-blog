6d6db627e297af2468254d29be20ce6f1f76c5fa
HarishLakshmanan <your-harish.lakshmanan@outlook.com>
2025-05-19 19:25:41 +0530

    deployment update

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react'
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import {useSelector} from 'react-redux'
 import { Link } from 'react-router-dom'
-import { set } from 'mongoose';
 
 export default function DashPost() {
    
